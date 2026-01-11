bf6c7a607f837192058ac8972ea8f15fecb74459
Rohit kumar <1009rohitkumar@gmail.com>
2026-01-11 10:35:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (7.26%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,8 +1,9 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         carry = 1
+        m = len(digits) - 1
 
-        for i in range(len(digits) - 1, -1, -1):
+        for i in range(m, -1, -1):
             digits[i] += carry
 
             if digits[i] < 10:
@@ -14,6 +15,9 @@ class Solution:
 
         if carry == 1:
             digits.insert(0, 1)
+
+        return digits
+
 
         return digits
 
