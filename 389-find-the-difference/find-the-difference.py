0cec8375b96defa76dbc73611758f1074b3bb044
Rohit kumar <1009rohitkumar@gmail.com>
2026-01-19 22:21:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (19.41%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -2,6 +2,6 @@ class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         res=0
         for ch in s+t:
-            res^=ord(ch)
-        return chr(res)
+            res^=ord(ch) #doing xor so every element which is not present in either srting will store in res
+        return chr(res) #"chr" convert it ascii to char
         
