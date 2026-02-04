861ab381ce9164c0effb9117e023da4f5848d9fe
Rohit kumar <1009rohitkumar@gmail.com>
2026-02-04 23:33:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (27.42%) - LeetSync

---
+++ 492-construct-the-rectangle/construct-the-rectangle.py
@@ -0,0 +1,6 @@
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+        for w in range(int(math.sqrt(area)), 0, -1):
+            if area % w == 0:
+                l = area // w
+                return [l, w]
